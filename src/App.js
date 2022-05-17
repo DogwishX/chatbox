8dfa78fde76212ae039878e32241fb329be86f90
Dogwish <zefiru@protonmail.com>
2022-05-17 05:44:26 +0100

    Change title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
+
 import "./App.css";
 import Home from "./Pages/Home/Home";
 
 import { Link } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    document.title = "PicSkin";
+  }, []);
+
   return (
     <div className="App">
       <Home />
