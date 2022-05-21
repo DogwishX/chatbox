28c981f373831b3191fe0f6a0dbb4d29cdebc430
Dogwish <zefiru@protonmail.com>
2022-05-21 03:29:35 +0100

    Add and style new content

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -3,30 +3,31 @@ import "./HowItWorks.css";
 function HowItWorks() {
   return (
     <div className="how-it-works">
-      <h2 className="how-it-works__title">
-        Como funciono?
-      </h2>
-      <h3 className="how-it-works__subtitle">
-        Fui criada para ser sua conselheira
-        virtual na compra de produtos para pele
-      </h3>
-      <p className="how-it-works__text">
-        Eiii, cheguei para te ajudar! Eu sou a
-        PicSkin sua conselheira virtual que te
-        ajuda a escolher os produtos mais
-        indicados para seu tipo de pele. Agora vou
-        te contar como funciono, basta voce me
-        chamar pela caixinha de diálogo aqui do
-        lado e iniciar uma conversa comigo. Por lá
-        vou te fazer algumas perguntas para
-        descobrir qual seu tipo de pele, e logo em
-        seguida vou te dar as MELHORES opções de
-        produtos de skincare. Seremos ótimos
-        amigos e eu vou estar sempre te ajudando
-        naquele momento de dúvida em que voce fica
-        olhando para a prateleira da fármacia sem
-        saber o que comprar.
-      </p>
+      <div className="how-it-works__content">
+        <h2 className="how-it-works__title">Como funciono?</h2>
+        <h3 className="how-it-works__subtitle">
+          Fui criada para ser sua conselheira virtual na compra de produtos para
+          pele
+        </h3>
+        <p className="how-it-works__text">
+          Eiii, cheguei para te ajudar! <br />
+          Eu sou a PicSkin sua conselheira virtual que te ajuda a escolher o
+          produto mais indicado para seu tipo de pele! E como eu faço isso!{" "}
+          <br />
+          É simples, a minha criadora me fez com todo o conhecimento de
+          dermatologia, por isso uso esse conhecimento que ela colocou em mim
+          para te orientar a fazer escolhas certas e seguras! <br />
+          Agora vou te contar como funciono, basta você me chamar pela caixinha
+          de diálogo aqui do lado e iniciar uma conversa comigo. Por lá vou te
+          fazer algumas perguntas para descobrir qual seu tipo de pele, e logo
+          em seguida vou te dar as MELHORES opções de produtos de skincare.
+          Seremos ótimos amigos e eu vou estar sempre te ajudando naquele
+          momento de dúvida em que você fica olhando para a prateleira da
+          farmácia sem saber o que comprar. <br />
+          Ah!!! E só te lembrando isso não é uma consulta médica viu, o contato
+          com seu médico é importante para intervenções mais precisas!
+        </p>
+      </div>
     </div>
   );
 }
