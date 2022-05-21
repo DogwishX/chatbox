615ae4af14feb3a10b252fdc8f37840d1611e147
Dogwish <zefiru@protonmail.com>
2022-05-21 02:33:06 +0100

    Remove title change on useEffect and unused link

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import { useEffect } from "react";
 import "./App.css";
 import Home from "./Pages/Home/Home";
 
-import { Link } from "react-router-dom";
-
 function App() {
-  useEffect(() => {
-    document.title = "PicSkin";
-  }, []);
-
   return (
     <div className="App">
       <Home />
